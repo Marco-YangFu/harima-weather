4786b88ec26bc503cfb480a20514a112fd53f102
Marco-YangFu <l0tus.yang.dage@gmail.com>
2025-08-26 22:35:35 +0900

    feat: add /weather screen and navigation link

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
-import { View, Text} from 'react-native';
+import { View, Text, Button } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
     return (
-     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>It works.（まずは空っぽ）
-        </Text>
-     </View>
-    );
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>It works.（まずは空っぽ）</Text>
+            <Link href="/weather" asChild>
+                <Button title="Go to Weather" />
+            </Link>
+        </View>
+    )
 }
